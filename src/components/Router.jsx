83e4b9419a8498655516e2a3a8974f611d250581
secgyu <abc2232002@gmail.com>
2024-12-15 17:21:04 +0900

    라우터 적용

---
+++ src/components/Router.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../routes/Home";
+import Auth from "../routes/Auth";
+
+const AppRouter = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return <Routes>{isLoggedIn ? <Route path="/" element={<Home />} /> : <Route path="/" element={<Auth />} />}</Routes>;
+};
+
+export default AppRouter;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import App from "./components/App.jsx";
 import firebase from "./firebase.js";
+import { BrowserRouter } from "react-router-dom";
 console.log(firebase);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
