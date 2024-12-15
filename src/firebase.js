602b4835caedb2b2e019a88b156b9a27498f7ffa
secgyu <abc2232002@gmail.com>
2024-12-15 14:19:13 +0900

    Update: 전체 파일 수정 및 Firebase 설정 추가

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyASTjhDS34OS_CeH4IEla4e6h-gZe_Pi-Q",
+  authDomain: "twitter-e3301.firebaseapp.com",
+  projectId: "twitter-e3301",
+  storageBucket: "twitter-e3301.firebasestorage.app",
+  messagingSenderId: "247119875359",
+  appId: "1:247119875359:web:56d620d1f92a91e8a1f42a",
+};
+
+export default firebase.initializeApp(firebaseConfig);
